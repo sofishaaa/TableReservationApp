e1c37f8adb6723b3fff28023d75452af006fb11a
sofishaa <sofiikahunkalo@gmail.com>
2024-02-04 07:04:03 +0200

    Add App files and folders

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TableReservationApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Reservation manager = new Reservation();
+            manager.AddRestaurant("A", 10);
+            manager.AddRestaurant("B", 5);
+
+            Console.WriteLine(manager.BookTable("A", new DateTime(2023, 12, 25), 3));
+            Console.WriteLine(manager.BookTable("A", new DateTime(2023, 12, 25), 3));
+
+        }
+    }
+}
---
+++ Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TableReservationApp
+{
+    public class Restaurant
+    {
+        public string name;
+        public Table[] table;
+    }
+}
